8126a8d60c9d00ca6f953577e7b20817c64b2ab4
Alex Sardan <sardanalex@gmail.com>
2013-03-23 17:42:04 +0200

    Test app for enumerating DLLs

---
+++ researchApps/testapp1/testapp1/main.c
@@ -0,0 +1,100 @@
+#include <windows.h> 
+#include <tlhelp32.h> 
+#include <tchar.h> 
+#include <stdio.h>
+ 
+//  Forward declarations: 
+BOOL ListProcessModules( DWORD dwPID ); 
+void printError( TCHAR* msg ); 
+ 
+int main( void )
+{
+	MEMORY_BASIC_INFORMATION meminfo;
+	ULONG addr = 0x00400000;
+	int i;
+	printf("Hello world!\n");
+	//for (i = 0; i < 3; i++)
+	{
+		if (!VirtualQuery((LPCVOID) addr, &meminfo, sizeof(MEMORY_BASIC_INFORMATION)))
+		{
+			printf("Failed with error %d.\n", GetLastError());
+			return -1;
+		}
+
+		printf ("Type:%x", meminfo.RegionSize);
+		addr = (ULONG)meminfo.BaseAddress + meminfo.RegionSize;
+	}
+  //ListProcessModules(GetCurrentProcessId() );
+  getchar();
+  return 0;
+}
+
+BOOL ListProcessModules( DWORD dwPID ) 
+{ 
+  HANDLE hModuleSnap = INVALID_HANDLE_VALUE; 
+  MODULEENTRY32 me32; 
+ 
+//  Take a snapshot of all modules in the specified process. 
+  hModuleSnap = CreateToolhelp32Snapshot( TH32CS_SNAPMODULE, dwPID ); 
+  if( hModuleSnap == INVALID_HANDLE_VALUE ) 
+  { 
+    printError( TEXT("CreateToolhelp32Snapshot (of modules)") ); 
+    return( FALSE ); 
+  } 
+ 
+//  Set the size of the structure before using it. 
+  me32.dwSize = sizeof( MODULEENTRY32 ); 
+ 
+//  Retrieve information about the first module, 
+//  and exit if unsuccessful 
+  if( !Module32First( hModuleSnap, &me32 ) ) 
+  { 
+    printError( TEXT("Module32First") );  // Show cause of failure 
+    CloseHandle( hModuleSnap );     // Must clean up the snapshot object! 
+    return( FALSE ); 
+  } 
+ 
+//  Now walk the module list of the process, 
+//  and display information about each module 
+  do 
+  { 
+    _tprintf( TEXT("\n\n     MODULE NAME:     %s"),             me32.szModule ); 
+    _tprintf( TEXT("\n     executable     = %s"),             me32.szExePath ); 
+    _tprintf( TEXT("\n     process ID     = 0x%08X"),         me32.th32ProcessID ); 
+    _tprintf( TEXT("\n     ref count (g)  =     0x%04X"),     me32.GlblcntUsage ); 
+    _tprintf( TEXT("\n     ref count (p)  =     0x%04X"),     me32.ProccntUsage ); 
+    _tprintf( TEXT("\n     base address   = 0x%08X"), (DWORD) me32.modBaseAddr ); 
+    _tprintf( TEXT("\n     base size      = %d"),             me32.modBaseSize ); 
+ 
+  } while( Module32Next( hModuleSnap, &me32 ) ); 
+
+    _tprintf( TEXT("\n"));
+ 
+//  Do not forget to clean up the snapshot object. 
+  CloseHandle( hModuleSnap ); 
+  return( TRUE ); 
+} 
+ 
+ 
+void printError( TCHAR* msg )
+{
+  DWORD eNum;
+  TCHAR sysMsg[256];
+  TCHAR* p;
+
+  eNum = GetLastError( );
+  FormatMessage( FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+         NULL, eNum,
+         MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // Default language
+         sysMsg, 256, NULL );
+
+  // Trim the end of the line and terminate it with a null
+  p = sysMsg;
+  while( ( *p > 31 ) || ( *p == 9 ) )
+    ++p;
+  do { *p-- = 0; } while( ( p >= sysMsg ) &&
+                          ( ( *p == '.' ) || ( *p < 33 ) ) );
+
+  // Display the message
+  _tprintf( TEXT("\n  WARNING: %s failed with error %d (%s)"), msg, eNum, sysMsg );
+}
